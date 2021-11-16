c0067031c3ef2c1d0aaa62eae9abfc92675f106e
eunae <91525795+euneaaa@users.noreply.github.com>
2021-11-16 16:43:00 +0900

    Update BoardDetailServlet.java

--- src/main/java/com/koreait/board/BoardDetailServlet.java
+++ src/main/java/com/koreait/board/BoardDetailServlet.java
@@ -14,12 +14,16 @@ public class BoardDetailServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         String siboard = req.getParameter("iboard");
         int iboard = Integer.parseInt(siboard);
-
+        
         BoardVO param = new BoardVO();
         param.setIboard(iboard);
         BoardVO result = BoardDAO.selBoard(param);
+        int prevIboard = BoardDAO.selPrevIboard(param);
+        int nextIboard = BoardDAO.selNextIboard(param);
 
         req.setAttribute("detail", result);
+        req.setAttribute("prevIdx", prevIboard);
+        req.setAttribute("nextIdx", nextIboard);
         String path = "/WEB-INF/jsp/detail.jsp";
         req.getRequestDispatcher(path).forward(req,res);
     }
